d85b021cfbc5757b90af080d687f6a8e23e18363
Chukwuma Anyadike <chukwuma.anyadike@gmail.com>
2025-06-08 19:02:52 -0500

    fixed CORS issue

--- profilerAPI/src/main/java/com/profiler/server/profilerAPI/controller/ResumeController.java
+++ profilerAPI/src/main/java/com/profiler/server/profilerAPI/controller/ResumeController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +28,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("api/v1/profiler/resumes")
+@CrossOrigin(origins = "http://localhost:4200")
 
 public class ResumeController {
 	@Autowired
--- profilerAPI/src/main/java/com/profiler/server/profilerAPI/controller/UserController.java
+++ profilerAPI/src/main/java/com/profiler/server/profilerAPI/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("api/v1/profiler/users")
+@CrossOrigin(origins = "http://localhost:3000")
 
 public class UserController {
 	@Autowired UserService userService;
